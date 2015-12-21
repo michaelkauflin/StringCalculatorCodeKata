3042bc05542bca6deafc0181ed01f04651d000d7
Kauflin, Michael <michael.kauflin@phonak.com>
2015-12-21 15:18:38 +0100

    more tests added (negative numbers)

--- StringCalculator.Tests/StringCalculatorFixture.cs
+++ StringCalculator.Tests/StringCalculatorFixture.cs
@@ -8,6 +8,10 @@ namespace CodeKata.Tests
     {
         [TestCase("", ExpectedResult = 0)]
         [TestCase("1,2", ExpectedResult = 3)]
+        [TestCase("0", ExpectedResult = 0)]
+        [TestCase("1", ExpectedResult = 1)]
+        [TestCase("-1", ExpectedResult = -1)]
+        [TestCase("2,-1", ExpectedResult = 1)]
         public int AddTest(string input)
         {
             var stringCalculator = new StringCalculator();
